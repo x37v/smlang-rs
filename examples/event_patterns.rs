c68e5d579f86aab88890fc49dde072f8b67bbee3
Alex Norman <alex@x37v.info>
2021-10-21 09:00:19 -0700

    proposed new syntax

--- examples/event_patterns.rs
+++ examples/event_patterns.rs
@@ -2,8 +2,6 @@
 
 use smlang::statemachine;
 
-/// Note event data
-#[derive(PartialEq)]
 pub struct NoteEventData {
     /// num
     pub num: u8,
@@ -11,69 +9,83 @@ pub struct NoteEventData {
     pub vel: u8,
 }
 
-#[derive(PartialEq)]
-pub enum Button {
-    Shift { index: usize, down: bool },
-    Pad { index: usize, down: bool },
+pub struct Button {
+    index: usize,
+    down: bool,
 }
 
-#[derive(PartialEq)]
-#[allow(missing_docs)]
 pub enum Events {
     ButtonEvent(Button),
     NoteEvent(NoteEventData),
+    FooEvent,
 }
 
-statemachine! {
-    transitions: {
-        *State1 + ButtonEvent(Button::Pad { index: 1, .. }) / action = State3,
-        State1 + ButtonEvent(Button::Pad { index: 42, down: false }) / action = State2,
-        State3 + ButtonEvent(Button::Pad { index: 42, down: true }) / action = State1,
-        State1 + NoteEvent(NoteEventData) [guard] = State5,
-        State1 + ButtonEvent(Button::Shift{index: 1, down: true}) = State5
-        //State2 + ButtonEvent(Button::Pad)  / action = State1
-    }
+#[derive(Clone, PartialEq)]
+pub enum States {
+    State1,
+    State2,
+    State3(usize),
 }
 
 /// Context
 pub struct Context;
 
-impl StateMachineContext for Context {
-    fn action(&mut self, event_data: &Button) {
-        let (index, down) = match event_data {
-            Button::Shift { index, down } => (index, down),
-            Button::Pad { index, down } => (index, down),
-        };
-        println!("Got valid Button = {} {}", index, down);
+/*
+   statemachine! {
+       transitions: {
+            *State1 + ButtonEvent(Button) [event_data.down] / [self.action(event_data)] = State2,
+            State1 + ButtonEvent(Button) [!event_data.down] / [self.action(event_data)] = State3(20),
+            State1 + FooEvent = State3(30),
+            State3(usize) + ButtonEvent(Button { down: true, ..}) [!event_data.index < 20 && state_data < 20]
+                / [self.action(event_data); println!("foo {}", state_data)] = State3(self.action2(state_data, event_data)),
+            State3(usize) + ButtonEvent(Button { down: false, ..}) = State3(state_data),
+       }
+   }
+*/
+
+pub struct StateMachine {
+    state: States,
+}
+
+impl StateMachine {
+    fn action(&mut self, _button: &mut Button) {
+        //TODO
     }
-    fn guard(&mut self, event_data: &NoteEventData) -> Result<(), ()> {
-        Ok(())
+
+    fn action2(&mut self, state_data: usize, _button: &mut Button) -> usize {
+        state_data + 23
+    }
+    pub fn process_event(&mut self, mut event: Events) {
+        match self.state {
+            States::State1 => match event {
+                Events::ButtonEvent(ref mut event_data @ Button { .. }) if event_data.down => {
+                    self.action(event_data);
+                    self.state = States::State2;
+                }
+                Events::ButtonEvent(ref mut event_data @ Button { .. }) if !event_data.down => {
+                    self.action(event_data);
+                    self.state = States::State3(20);
+                }
+                Events::FooEvent => {
+                    self.state = States::State3(30);
+                }
+                _ => (),
+            },
+            States::State3(state_data) => match event {
+                Events::ButtonEvent(ref mut event_data @ Button { down: true, .. })
+                    if event_data.index < 20 && state_data < 20 =>
+                {
+                    self.action(event_data);
+                    self.state = States::State3(self.action2(state_data, event_data));
+                }
+                Events::ButtonEvent(ref mut event_data @ Button { down: false, .. }) => {
+                    self.state = States::State3(state_data);
+                }
+                _ => (),
+            },
+            _ => (),
+        }
     }
 }
 
-fn main() {
-    let mut sm = StateMachine::new(Context);
-    let result = sm.process_event(Events::ButtonEvent(Button::Pad {
-        index: 2,
-        down: true,
-    }));
-    assert!(result == Err(Error::InvalidEvent));
-
-    let result = sm.process_event(Events::ButtonEvent(Button::Pad {
-        index: 2084,
-        down: false,
-    }));
-    assert!(result == Err(Error::InvalidEvent));
-
-    let result = sm.process_event(Events::ButtonEvent(Button::Pad {
-        index: 1,
-        down: false,
-    }));
-    assert!(result == Ok(&States::State3));
-
-    let result = sm.process_event(Events::ButtonEvent(Button::Pad {
-        index: 42,
-        down: true,
-    }));
-    assert!(result == Ok(&States::State1));
-}
+fn main() {}
