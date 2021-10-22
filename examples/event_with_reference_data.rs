c5c798b7dccebfd6984719f4fa81ff3aab873d43
Alex Norman <alex@x37v.info>
2021-10-21 22:12:54 -0700

    transated another example

--- examples/event_with_reference_data.rs
+++ examples/event_with_reference_data.rs
@@ -2,44 +2,39 @@
 //!
 //! A simple example of a state machine which will get events that contain references.
 
-#![deny(missing_docs)]
-
 use smlang::statemachine;
 
 /// Reference wrapper
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct MyReferenceWrapper<'a>(pub &'a u32);
 
+pub enum Events<'a, 'b> {
+    Event1(&'a [u8]),
+    Event2(MyReferenceWrapper<'b>),
+}
+
 statemachine! {
     transitions: {
-        *State1 + Event1(&'a [u8]) [guard1] / action1 = State2,
-        State2 + Event2(MyReferenceWrapper<'b>) [guard2] / action2 = State3,
+        *State1 + Event1(_) [ctx.guard1(event_data)] / {ctx.action1(event_data)} = State2,
+        State2 + Event2(_) [ctx.guard2(event_data)] / {ctx.action2(event_data)} = State3,
     }
 }
 
 /// Context
 pub struct Context;
 
-impl StateMachineContext for Context {
-    fn guard1(&mut self, event_data: &[u8]) -> Result<(), ()> {
+impl Context {
+    fn guard1(&mut self, event_data: &[u8]) -> bool {
         // Only ok if the slice is not empty
-        if !event_data.is_empty() {
-            Ok(())
-        } else {
-            Err(())
-        }
+        !event_data.is_empty()
     }
 
     fn action1(&mut self, event_data: &[u8]) {
         println!("Got valid Event Data = {:?}", event_data);
     }
 
-    fn guard2(&mut self, event_data: &MyReferenceWrapper) -> Result<(), ()> {
-        if *event_data.0 > 9000 {
-            Ok(())
-        } else {
-            Err(())
-        }
+    fn guard2(&mut self, event_data: &MyReferenceWrapper) -> bool {
+        *event_data.0 > 9000
     }
 
     fn action2(&mut self, event_data: &MyReferenceWrapper) {
@@ -51,13 +46,13 @@ fn main() {
     let mut sm = StateMachine::new(Context);
 
     let result = sm.process_event(Events::Event1(&[])); // Guard will fail
-    assert!(result == Err(Error::GuardFailed(())));
+    assert!(result == Err(Error::InvalidEvent));
     let result = sm.process_event(Events::Event1(&[1, 2, 3])); // Guard will pass
     assert!(result == Ok(&States::State2));
 
     let r = 42;
     let result = sm.process_event(Events::Event2(MyReferenceWrapper(&r))); // Guard will fail
-    assert!(result == Err(Error::GuardFailed(())));
+    assert!(result == Err(Error::InvalidEvent));
 
     let r = 9001;
     let result = sm.process_event(Events::Event2(MyReferenceWrapper(&r))); // Guard will pass
