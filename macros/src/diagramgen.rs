40d90af1e407c7b5ecce54766c192bf6ed908097
Alex Norman <alex@x37v.info>
2021-10-25 09:05:34 -0700

    add data into states

--- macros/src/diagramgen.rs
+++ macros/src/diagramgen.rs
@@ -1,6 +1,6 @@
 use crate::parser::*;
 use quote::quote;
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
 fn escape(v: String) -> String {
     let mut out = v.clone();
@@ -15,13 +15,31 @@ fn escape(v: String) -> String {
 pub fn generate_diagram(sm: &ParsedStateMachine) -> String {
     let mapping = &sm.states_events_mapping;
 
-    let diagram_states = sm.states.iter().map(|s| s.0);
+    let mut diagram_states: HashMap<String, String> = HashMap::new();
+
+    for (state, events) in mapping {
+        for eventmapping in events {
+            let in_state = &eventmapping.in_state;
+            diagram_states.insert(state.clone(), escape(quote! { #in_state }.to_string()));
+
+            //add out state, only if it isn't already there as in state has the data
+            let state = eventmapping.out_state.to_string();
+            if !diagram_states.contains_key(&state) {
+                diagram_states.insert(state.clone(), state.clone());
+            }
+        }
+    }
+
     let mut diagram_events = VecDeque::new();
     let mut diagram_transitions = vec![];
     let mut index = 1;
     for (state, events) in mapping {
         for eventmapping in events {
-            let mut label = eventmapping.event.to_string();
+            let in_state = diagram_states.get(state).unwrap();
+            let out_state = eventmapping.out_state.to_string();
+
+            let mut label = in_state.clone();
+            label += format!(" + {}", eventmapping.event).as_str();
 
             if let Some(p) = &eventmapping.event_pattern {
                 label += format!("({})", escape(quote! {#p}.to_string())).as_str();
@@ -34,22 +52,26 @@ pub fn generate_diagram(sm: &ParsedStateMachine) -> String {
             if let Some(actions) = &eventmapping.actions {
                 label += format!(" / {}", escape(quote! {#actions}.to_string())).as_str();
             };
-            label += format!(" = {}", eventmapping.out_state.to_string()).as_str();
+            label += format!(" = {}", out_state).as_str();
             if let Some(e) = &eventmapping.out_state_data_expr {
                 label += quote! {(#e)}.to_string().as_str();
             };
 
-            let slabel = format!("{}", index);
-            diagram_transitions.push((state, eventmapping.out_state.to_string(), slabel.clone()));
-            diagram_events.push_front((slabel, label));
+            let out_state = diagram_states.get(&out_state).unwrap();
+            let indexl = format!("{}", index);
+
+            //in, out, label
+            diagram_transitions.push((in_state.clone(), out_state.clone(), indexl.clone()));
+            diagram_events.push_front((indexl, label));
             index += 1;
         }
     }
 
     let state_string = diagram_states
+        .values()
         .map(|s| {
             format!(
-                "\t{} [shape=box color=\"red\" fillcolor=\"#ffbb33\" style=filled]",
+                "\t\"{}\" [shape=box color=\"red\" fillcolor=\"#ffbb33\" style=filled]",
                 s
             )
         })
@@ -60,7 +82,12 @@ pub fn generate_diagram(sm: &ParsedStateMachine) -> String {
         .collect::<Vec<String>>();
     let transition_string = diagram_transitions
         .iter()
-        .map(|t| format!("\t{0} -> {1} [color=blue label={2}];", t.0, t.1, t.2))
+        .map(|t| {
+            format!(
+                "\t\"{0}\" -> \"{1}\" [color=blue label={2}];",
+                t.0, t.1, t.2
+            )
+        })
         .collect::<Vec<String>>();
 
     format!(
