b612921ed8368f3f0f907bcc1696fcbef711307f
Alex Norman <alex@x37v.info>
2021-10-25 16:56:25 -0700

    allow for initial state with data, create default

--- examples/state_with_data.rs
+++ examples/state_with_data.rs
@@ -7,7 +7,7 @@
 use smlang::statemachine;
 
 /// State data
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Default)]
 pub struct MyStateData(pub u32);
 
 ///Events
@@ -22,8 +22,13 @@ pub enum Events {
 
 statemachine! {
     transitions: {
-        *State1 + Event1 = State2(MyStateData(42)),
-        State2(MyStateData) + Event2 [state_data.0 == 42] = State1,
+        *State1(MyStateData) + Event1 = State2(MyStateData(42)),
+        State2(MyStateData) + Event2 [state_data.0 == 42] = State1(MyStateData(2084)),
+        State2(MyStateData) + Event2 [state_data.0 == 2084] = State3(1),
+
+        //hack to get around not being able to have data with terminal state..
+        //add a transition that will never happen (guard is false)
+        State3(usize) + Event1 [false] = State3(3)
         // ...
     }
 }
