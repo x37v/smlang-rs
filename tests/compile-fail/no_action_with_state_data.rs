6c2021129d35062872e460e945e54536e9bb61a0
Alex Norman <alex@x37v.info>
2021-10-25 17:13:17 -0700

    no issue with double anymore, updated tests

--- tests/compile-fail/no_action_with_state_data.rs
+++ tests/compile-fail/no_action_with_state_data.rs
@@ -2,6 +2,13 @@ extern crate smlang;
 
 use smlang::statemachine;
 
+#[derive(PartialEq)]
+pub enum Events {
+    Event1,
+}
+
+pub struct Context;
+
 statemachine! {
     transitions: {
         *State1 + Event1 = State2(u32), //~ This state has data associated, but not action is define here to provide it.
@@ -9,4 +16,3 @@ statemachine! {
 }
 
 fn main() {}
-
--- tests/compile-fail/no_starting_state.rs
+++ tests/compile-fail/no_starting_state.rs
@@ -2,7 +2,15 @@ extern crate smlang;
 
 use smlang::statemachine;
 
-statemachine! { 
+#[derive(PartialEq)]
+pub enum Events {
+    Event1,
+    Event2,
+}
+
+pub struct Context;
+
+statemachine! {
     transitions: {
         //~ ERROR No starting state defined, indicate the starting state with a *
         State1 + Event1 = State2,
