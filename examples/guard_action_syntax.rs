aa2036829b60b22bda2daa4b26d9977c32ae8b65
Alex Norman <alex@x37v.info>
2021-10-28 07:35:25 -0700

    re-enable example

--- examples/guard_action_syntax.rs
+++ examples/guard_action_syntax.rs
@@ -2,7 +2,6 @@
 //!
 //! An example of using guards and actions with state and event data.
 
-/*
 #![deny(missing_docs)]
 
 use smlang::statemachine;
@@ -15,39 +14,48 @@ pub struct MyEventData(pub u32);
 #[derive(PartialEq)]
 pub struct MyStateData(pub u32);
 
+#[derive(PartialEq)]
+///Events
+pub enum Events {
+    ///Event1 with data
+    Event1(MyEventData),
+    ///Event2
+    Event2,
+}
+
+///guard as a function
+pub fn guard(_event_data: &MyEventData) -> bool {
+    todo!()
+}
+
+///action as a function
+pub fn action(_event_data: &MyEventData) {
+    todo!()
+}
+
 statemachine! {
     transitions: {
-        *State1 + Event1(MyEventData) [guard1] / action1 = State2,
-        State2(MyStateData) + Event2  [guard2] / action2 = State3,
-        // ...
+        *State1 + Event1(_) [guard(event)] / action(event); = State2(MyStateData(2)),
+        State1 + Event2 [false] / println!("won't happen"); = State2(MyStateData(2084)),
+        State2(MyStateData) + Event2 [ctx.guard(state)] / ctx.action(state); = State3,
+        State2(MyStateData) + Event1(_) [true] / { println!("multiple"); println!("lines)") } = State3,
     }
 }
 
 /// Context
 pub struct Context;
 
-impl StateMachineContext for Context {
-    // Guard1 has access to the data from Event1
-    fn guard1(&mut self, _event_data: &MyEventData) -> Result<(), ()> {
-        todo!()
+impl Context {
+    ///context method guard
+    fn guard(&mut self, _s: &MyStateData) -> bool {
+        true
     }
 
-    // Action1 has access to the data from Event1, and need to return the state data for State2
-    fn action1(&mut self, _event_data: &MyEventData) -> MyStateData {
-        todo!()
-    }
-
-    // Guard2 has access to the data from State2
-    fn guard2(&mut self, _state_data: &MyStateData) -> Result<(), ()> {
-        todo!()
-    }
-
-    // Action2 has access to the data from State2
-    fn action2(&mut self, _state_data: &MyStateData) {
+    ///context method action
+    fn action(&mut self, _s: &MyStateData) {
         todo!()
     }
 }
-*/
 
 fn main() {
     //TODO?
